6278b96d16b095ec09ec01b8b8734b8814807b79
Ken Makhanu <kennedy.makhanu@gmail.com>
2021-03-25 09:04:19 -0400

    changes again

--- pipeline.tf
+++ pipeline.tf
@@ -8,9 +8,10 @@ resource "aws_codebuild_project" "tf-plan" {
   }
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:latest"
+    image                       = "hashicorp/terraform:0.14.4"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
+    privileged_mode             = true
     registry_credential {
         credential = var.dockerhub_credentials
         credential_provider = "SECRETS_MANAGER"
@@ -43,6 +44,7 @@ resource "aws_codebuild_project" "tf-apply" {
   name          = "tf-cicd-apply"
   description   = "Apply stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
+
   artifacts {
     type = "CODEPIPELINE"
   }
@@ -51,6 +53,7 @@ resource "aws_codebuild_project" "tf-apply" {
     image                       = "hashicorp/terraform:0.14.4"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
+    privileged_mode             = true
     registry_credential {
         credential = var.dockerhub_credentials
         credential_provider = "SECRETS_MANAGER"
