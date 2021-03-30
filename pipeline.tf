874783e0d9f363b4d42320515879abe8032b87ca
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2021-03-29 21:45:44 -0400

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -102,7 +102,7 @@ resource "aws_codepipeline" "cicd-pipeline"{
       name            = "Build"
       category        = "Build"
       provider        = "CodeBuild"
-      owner            = "Kendrewhomesolutions"
+      owner            = "AWS"
       input_artifacts = ["tf-code"]
       version         = "1"
       configuration = {
