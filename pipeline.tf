2f8d954d873b79ea8ebde829d4fd991b10ceaacc
Ken Makhanu <kennedy.makhanu@gmail.com>
2021-04-09 13:59:10 -0400

    again

--- pipeline.tf
+++ pipeline.tf
@@ -44,7 +44,7 @@ resource "aws_codebuild_project" "tf-apply" {
   }
   source  {
     type   = "CODEPIPELINE"
-    buildspec = file("buildspec/plan-buildspec.yml")
+    buildspec = file("buildspec/apply-buildspec.yml")
   }
 }
 
