a4b39d0ef3cfb1fe578d68848592e301f37ce088
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2021-04-18 10:31:27 -0400

    Delete 3variables.tf

--- 3variables.tf
+++
@@ -1,5 +0,0 @@
-variable "subnet_cidrs_public" {
-  description = "Subnet CIDRs for public subnets (length must match configured availability_zones)"
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-  type = "list"
-}
